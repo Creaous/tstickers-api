0323ff49ad43fc06fa6af682dccdeda0631db211
Creaous <68230846+Creaous@users.noreply.github.com>
2023-05-16 19:57:59 +0930

    Update code

--- README.md
+++ README.md
@@ -1,57 +1,28 @@
-# Telegram Stickers API
-
-This is basically a wrapper around the Telegram Bot API that only provides access to sticker data.
-
-It aggressively caches sticker image data both on-disk and in the `Cache-Control` header, both to save requests to Telegram and to save requests from clients.
-
-It is suitable for public use -- no API tokens are required.
+# Telegram Sticker API
+This is basically a wrapper around the Telegram Bot API that only provides access to sticker data. It aggressively caches sticker image data both on-disk and in the `Cache-Control` header, both to save requests to Telegram and to save requests from clients.
 
 ## Features
-
 - Public sticker API
 - Automatically handles WebP -> PNG conversion
 - Aggressive file caching for fast response times
 - Pre-set CORS headers -- use it in client-side JS immediately
 
 ## Usage
-
-Create `config.json`:
-
-```json
-{
-  "BOT_TOKEN": "Something",
-  "CACHE_TIME": 3600
-}
-```
+Rename `config.example.json` to `config.json` and edit the values as required.
 
 Then:
-
 ```shell
-npm install
-node index
+npm i
+npm start
 ```
 
-In production, you can use environment variables instead of `config.json`.
+## Endpoints
+### **GET** /pack/:name
+Returns a sticker pack's data.
 
-Then, simply use these endpoints:
+Example: `https://example.com/pack/Animals`
 
-```
-GET /pack/:name
-GET /sticker/:id.png
-```
+### **GET** /sticker/:id.png
+Returns a valid sticker image.
 
-Examples:
-- https://stickers.horner.tj/pack/Animals
-- https://stickers.horner.tj/sticker/CAADAgAD3gAD9HsZAAG9he9u98XOPQI.png
-
-**(Please do not use stickers.horner.tj in production. It is not stable and may go down at any time. It is simply for demonstration purposes.)**
-
-## Docker
-
-A `Dockerfile` is included in this repo, but prebuilt images are available:
-
-```
-docker pull tjhorner/tstickers-api:latest
-```
-
-For your reference, a sample `docker-compose.yml` is also included.
+Example: `https://example.com/sticker/CAADAgAD3gAD9HsZAAG9he9u98XOPQI.png`
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var config
 try {
-  config = require('./config.json')
-} catch(e) {
-  config = process.env
+	config = require('./config.json')
+} catch (e) {
+	config = process.env
 }
 
 const path = require('path')
@@ -22,67 +22,67 @@ const cors = require('cors')
 
 const sharp = require('sharp')
 
-const cachedLinks = { }
+const cachedLinks = {}
 
 app.use(cacheControl())
 app.use(cors())
 
 app.get("/pack/:name", async (req, res) => {
-  var packRaw
+	var packRaw
 
-  try {
-    packRaw = await bot.getStickerSet(req.params.name)
-  } catch(e) {
-    res.status(500)
-    res.send({ error: "Something bad happened. Try again later." })
-    return
-  }
+	try {
+		packRaw = await bot.getStickerSet(req.params.name)
+	} catch (e) {
+		res.status(500)
+		res.send({ error: "Something bad happened. Try again later." })
+		return
+	}
 
-  res.cacheControl = {
-    maxAge: 120,
-    public: true
-  }
+	res.cacheControl = {
+		maxAge: 120,
+		public: true
+	}
 
-  var pack = {
-    name: packRaw.title,
-    masks: packRaw.contains_masks,
-    stickers: packRaw.stickers.map(sticker => {
-      return {
-        id: sticker.file_id,
-        size: sticker.file_size,
-        width: sticker.width,
-        height: sticker.height,
-        emoji: sticker.emoji,
-        mask_position: sticker.mask_position
-      }
-    })
-  }
+	var pack = {
+		name: packRaw.title,
+		masks: packRaw.contains_masks,
+		stickers: packRaw.stickers.map(sticker => {
+			return {
+				id: sticker.file_id,
+				size: sticker.file_size,
+				width: sticker.width,
+				height: sticker.height,
+				emoji: sticker.emoji,
+				mask_position: sticker.mask_position
+			}
+		})
+	}
 
-  res.send(pack)
+	res.send(pack)
 })
 
 app.get("/sticker/:id.png", async (req, res) => {
-  var stickerPng
+	var stickerPng
 
-  try {
-    const stickerUrl = cachedLinks[req.params.id] || (cachedLinks[req.params.id] = await bot.getFileLink(req.params.id))
-    const stickerWebp = await cache.getImage(stickerUrl)
-  
-    stickerPng = await sharp(stickerWebp).toFormat("png").toBuffer()
-  } catch(e) {
-    res.setHeader("Content-Type", "image/jpeg")
-    res.status(500)
-    res.sendFile(path.join(__dirname, "static", "500.jpg"))
-    return
-  }
+	try {
+		const stickerUrl = cachedLinks[req.params.id] || (cachedLinks[req.params.id] = await bot.getFileLink(req.params.id))
+		const stickerWebp = await cache.getImage(stickerUrl)
 
-  res.cacheControl = {
-    maxAge: parseInt(config.CACHE_TIME),
-    public: true
-  }
+		stickerPng = await sharp(stickerWebp).toFormat("png").toBuffer()
+	} catch (e) {
+		res.setHeader("Content-Type", "image/jpeg")
+		res.status(500)
+		res.sendFile(path.join(__dirname, "static", "500.jpg"))
+		return
+	}
 
-  res.setHeader("Content-Type", "image/png")
-  res.send(stickerPng)
+	res.cacheControl = {
+		maxAge: parseInt(config.CACHE_TIME),
+		public: true
+	}
+
+	res.setHeader("Content-Type", "image/png")
+	res.send(stickerPng)
 })
 
-app.listen(process.env.PORT || 3000)
+app.listen(config.PORT || 3000)
